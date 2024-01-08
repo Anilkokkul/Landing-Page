01c0a6a2caf00d89613c1b539f595b20e84fccb0
Anilkokkul <anilkokkul1997@gmail.com>
2024-01-08 10:47:07 +0530

    Update navbar and announcement bar styling and positioning.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Testimonials from "./components/Testimonials";
 function App() {
   return (
     <div>
-      <div className=" h-md-10 py-2 px-5 border-b">
+      <div className=" h-md-10 py-2 px-5 border-b bg-white relative z-30">
         <div className=" text-center flex justify-center items-center gap-3 font-bold text-sm ">
           <span className=" py-1 px-2 bg-slate-200 rounded-xl text-xs h-6">
             Announcement
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,65 +9,73 @@ function Navbar() {
     setToggle(!toggle);
   };
   return (
-    <div className=" py-4 px-6 md:px-20 lg:px-28 border-b">
-      <div className="md:px-18 h-10 flex justify-between  items-center font-bold">
-        <h1 className=" text-2xl font-extrabold tracking-wide">Mode UI</h1>
-        <div>
-          <ul className="lg:flex gap-4 opacity-70 hidden ">
-            <li>
-              Card Access{" "}
-              <img src="/chevron-down.png" alt="down" className=" inline" />{" "}
-            </li>
-            <li className=" cursor-pointer">Banking</li>
-            <li className=" cursor-pointer">Processing</li>
-            <li className=" cursor-pointer">About</li>
-            <li className=" cursor-pointer">Carrier</li>
-            <li className=" cursor-pointer">Contact</li>
-          </ul>
-        </div>
-        <div className="flex items-center gap-5">
-          <button className=" bg-slate-200 px-2 rounded-md p-1 flex items-center gap-2">
-            <span>Login</span> <img src="/icons.png" alt="login" />{" "}
-          </button>
-          <div
-            className=" font-bold text-2xl lg:hidden transition duration-1000 ease-in-out"
-            onClick={handleToggle}
-          >
-            {toggle ? (
-              <div>
-                <MdOutlineClose />
-              </div>
-            ) : (
-              <div>
-                <AiOutlineMenu />
-              </div>
-            )}
+    <div className="relative ">
+      <div className=" py-4 px-6 md:px-20 lg:px-28 border-b relative z-30 bg-white">
+        <div className="md:px-18 h-10 flex justify-between  items-center font-bold">
+          <h1 className=" text-2xl font-extrabold tracking-wide">Mode UI</h1>
+          <div>
+            <ul className="lg:flex gap-4 opacity-70 hidden ">
+              <li>
+                Card Access{" "}
+                <img src="/chevron-down.png" alt="down" className=" inline" />{" "}
+              </li>
+              <li className=" cursor-pointer">Banking</li>
+              <li className=" cursor-pointer">Processing</li>
+              <li className=" cursor-pointer">About</li>
+              <li className=" cursor-pointer">Carrier</li>
+              <li className=" cursor-pointer">Contact</li>
+            </ul>
+          </div>
+          <div className="flex items-center gap-5">
+            <button className=" bg-slate-200 px-2 rounded-md p-1 flex items-center gap-2">
+              <span>Login</span> <img src="/icons.png" alt="login" />{" "}
+            </button>
+            <div
+              className="font-bold text-2xl lg:hidden"
+              onClick={handleToggle}
+            >
+              {toggle ? (
+                <div>
+                  <MdOutlineClose />
+                </div>
+              ) : (
+                <div>
+                  <AiOutlineMenu />
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
       <div
-        className={`flex justify-end toggle ${
-          toggle ? "" : "hidden"
-        } lg:hidden`}
+        className={`lg:hidden w-[98%] mx-[1%] absolute z-20 transition-all duration-700 ease-in-out ${
+          toggle ? "top-20 opacity-100 " : "top-[-400px] opacity-0"
+        }  `}
       >
-        <ul className="text-start sm:max-w-96 bg-slate-300 absolute z-30 rounded-md transition overflow-hidden">
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 leading-8 px-3 tracking-wide cursor-pointer">
+        <ul
+          className={`text-start w-full bg-fuchsia-400 text-white  rounded-md transition-all overflow-hidden p-4 duration-900  `}
+        >
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 leading-8 px-3 tracking-wide cursor-pointer">
             Card Access{" "}
-            <img src="/chevron-down.png" alt="down" className=" inline" />
+            <img
+              src="/chevron-down.svg"
+              alt="down"
+              className=" inline"
+            />
           </li>
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
             Banking
           </li>
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
             Processing
           </li>
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
             About
           </li>
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
             Carrier
           </li>
-          <li className=" hover:bg-slate-600 hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
+          <li className=" hover:bg-fuchsia-600 rounded hover:text-white transition duration-700 h-8 px-3 leading-8 tracking-wide cursor-pointer">
             Contact
           </li>
         </ul>
